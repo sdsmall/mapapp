d7e5b374fc3169a9e732cfeeef2ffb057c52d3dd
Samantha Small <sds62@pitt.edu>
2014-12-27 12:57:54 -0500

    dynamic buttons from resource files

--- app/src/main/java/com/trial/sam/simplemap/ChoiceMenuActivity.java
+++ app/src/main/java/com/trial/sam/simplemap/ChoiceMenuActivity.java
@@ -1,56 +1,115 @@
 package com.trial.sam.simplemap;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.LightingColorFilter;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import maplistconverter.PointSet;
 
 public class ChoiceMenuActivity extends Activity {
+    Button b;
+    ScrollView scrollview;
+    ArrayList<InputStream> files;
+    ArrayList<PointSet> sets;
+    ArrayList<String> typeNames;
 
-    private Button b1, b2, b3;
 
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choice_menu);
 
-        b1=(Button)findViewById(R.id.computer_lab);
-        b1.getBackground().setColorFilter(new LightingColorFilter(0x001E359D, 0x001E359D));
+        getFiles();
 
-        b2=(Button)findViewById(R.id.print);
-        b2.getBackground().setColorFilter(new LightingColorFilter(0x001E359D, 0x001E359D));
+        filesToPointSets();
 
-        b3=(Button)findViewById(R.id.dining_dollars);
-        b3.getBackground().setColorFilter(new LightingColorFilter(0x001E359D, 0x001E359D));
+        scrollview = new ScrollView(this);
+        LinearLayout linearlayout = new LinearLayout(this);
+        linearlayout.setOrientation(LinearLayout.VERTICAL);
+
+        scrollview.addView(linearlayout);
+        this.setContentView(scrollview);
+
+        for(int jj = 0; jj<typeNames.size(); jj++)
+        {
+            LinearLayout linear1 = new LinearLayout(this);
+            linear1.setOrientation(LinearLayout.HORIZONTAL);
+            linearlayout.addView(linear1);
+            b = new Button(this);
+            b.setText(typeNames.get(jj));
+            b.setId(jj);
+
+            b.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT));
+
+            linear1.addView(b);
+
+            b.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    Toast.makeText(getApplicationContext(), "Yipee.."+ v.getId(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
+    public void getFiles(){
 
-    // Called when the user clicks the Navigate button
-    public void findComputerLab(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, MapsActivity.class);
-        intent.putExtra("pointType",0);
-        startActivity(intent);
+        AssetManager assetManager = getResources().getAssets();
+
+        String[] test = new String[0];
+        try {
+            test = assetManager.list("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<String> fileNames = new ArrayList<>();
+
+        for(int ii = 0; ii<test.length; ii++){
+            if(test[ii].contains(".txt")){
+                fileNames.add(test[ii]);
+            }
+        }
+
+        files = new ArrayList<>();
+        for(int jj = 0; jj<fileNames.size(); jj++){
+            try {
+                files.add(assetManager.open(fileNames.get(jj)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    // Called when the user clicks the Navigate button
-    public void findPrinter(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, MapsActivity.class);
-        intent.putExtra("pointType",1);
-        startActivity(intent);
-    }
+    public void filesToPointSets(){
+        sets = new ArrayList<>();
+        typeNames = new ArrayList<>();
 
-    // Called when the user clicks the Navigate button
-    public void findDiningDollarsLoc(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, MapsActivity.class);
-        intent.putExtra("pointType",2);
-        startActivity(intent);
+        for(int ii = 0; ii<files.size(); ii++){
+            try {
+                sets.add(new PointSet(files.get(ii)));
+                typeNames.add(sets.get(ii).getPointType());
+            }
+            catch(IOException e){
+
+            }
+        }
     }
 
 }
